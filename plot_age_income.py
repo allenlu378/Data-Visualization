35a7cca218e73d131f878d8f167a6e89495b58df
donpuz <lypuzon@ctemc.org>
2019-04-17 14:23:31 -0400

    Update code

--- plot_age_income.py
+++ plot_age_income.py
@@ -8,18 +8,28 @@ Current Population Survey.
 (c) 2014 Project Lead The Way
 '''
 import matplotlib.pyplot as plt
-
+import numpy as np
 # Get the income/age data from CSV
-datafile = open('municpal-building-energy-use-2009-2014.csv','r')
-data = datafile.readlines()
-
+municipal_file = open('municpal-building-energy-use-2009-2014.csv','r')
+municipal = municipal_file.readlines()
+consumption_file = open('municpal-building-energy-use-2009-2014.csv','r')
+consumption = consumption_file.readlines()
 ####
 # Transform the data from strings to signed integers
 ####
 years=[]
 energy_costs=[]
 total_GHG_arr = []
-for line in data[1:]: # Omit header lines
+energy_consumptions = []
+energy_source_consumption = []
+for line in consumption[5:]: # Omit header lines
+    category, energy_estimate = line.split(',')
+    energy_source_consumption.append(category)
+    energy_source_consumption.append(float(energy_estimate.rstrip())) # omit space, -, $, \n
+plt.barh(np.arange(len(energy_source_consumption)), energy_consumptions)
+
+
+for line in municipal[1:]: # Omit header lines
     id, name, address, GFA, year, site_EUI, WNS, source_EUI, WNS_EUI, electricity_use_kbtu, electricity_use_kwh, WNS_electricity, natural_gas_kbtu, natural_gas_therms, WNS_natural_gas, steam_use, total_GHG, GHG_intensity, energy_cost, energy_intensity = line.split(',')
     if energy_cost == 'Not Available':
         continue;
