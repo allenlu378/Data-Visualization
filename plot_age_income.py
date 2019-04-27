61029f46eda180eec3e4fc42a2cd402bab3fa991
allen <allenlu378@gmail.com>
2019-04-26 22:25:03 -0400

    Create Penn_Energy_Source

--- plot_age_income.py
+++ plot_age_income.py
@@ -9,10 +9,11 @@ Current Population Survey.
 '''
 import matplotlib.pyplot as plt
 import numpy as np
+import seaborn as sns
 # Get the income/age data from CSV
 municipal_file = open('municpal-building-energy-use-2009-2014.csv','r')
 municipal = municipal_file.readlines()
-consumption_file = open('municpal-building-energy-use-2009-2014.csv','r')
+consumption_file = open('Pennsylvania_Energy_Consumption_Estimates_2016.csv','r')
 consumption = consumption_file.readlines()
 ####
 # Transform the data from strings to signed integers
@@ -22,12 +23,6 @@ energy_costs=[]
 total_GHG_arr = []
 energy_consumptions = []
 energy_source_consumption = []
-for line in consumption[5:]: # Omit header lines
-    category, energy_estimate = line.split(',')
-    energy_source_consumption.append(category)
-    energy_source_consumption.append(float(energy_estimate.rstrip())) # omit space, -, $, \n
-plt.barh(np.arange(len(energy_source_consumption)), energy_consumptions)
-
 
 for line in municipal[1:]: # Omit header lines
     id, name, address, GFA, year, site_EUI, WNS, source_EUI, WNS_EUI, electricity_use_kbtu, electricity_use_kwh, WNS_electricity, natural_gas_kbtu, natural_gas_therms, WNS_natural_gas, steam_use, total_GHG, GHG_intensity, energy_cost, energy_intensity = line.split(',')
@@ -46,7 +41,7 @@ ax.set_ylabel('Total Energy Cost($)')
 
 fig.show()
 del years[:]
-for line in data[1:]: # Omit header lines
+for line in municipal[1:]: # Omit header lines
     id, name, address, GFA, year, site_EUI, WNS, source_EUI, WNS_EUI, electricity_use_kbtu, electricity_use_kwh, WNS_electricity, natural_gas_kbtu, natural_gas_therms, WNS_natural_gas, steam_use, total_GHG, GHG_intensity, energy_cost, energy_intensity = line.split(',')
     if total_GHG == 'Not Available':
         continue;
@@ -59,3 +54,16 @@ ax.set_xlabel('Year')
 ax.set_ylabel('Greenhouse Gas Emission(Metric Tons CO2e)')
 
 fig.show()
+for line in consumption[5:]: # Omit header lines
+    category, energy_estimate = line.split(',')
+    energy_source_consumption.append(category)
+    energy_consumptions.append(float(energy_estimate.rstrip())) # omit space, -, $, \n
+fig, ax = plt.subplots()
+y_pos = np.arange(len(energy_source_consumption))
+plt.barh(y_pos, energy_consumptions)
+ax.set_yticks(y_pos)
+ax.set_yticklabels(energy_source_consumption)
+ax.invert_yaxis()  # labels read top-to-bottom
+ax.set_xlabel('Energy Consumption(Trillion btu)')
+ax.set_title('Energy Consumption by Source in Pennsylvania')
+plt.show()
